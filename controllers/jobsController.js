4ea65550dc64ba3612f8171f6fbe7e15ff88569f
amanVar06 <amanvarshney212@gmail.com>
2023-02-14 15:13:32 +0530

    Added update route and find near jobs with in a radius

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 const Job = require("../models/jobs.js");
+const geoCoder = require("../utils/geocoder.js");
 
 // Get all Jobs => /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
@@ -19,5 +20,54 @@ exports.newJob = async (req, res, next) => {
     success: true,
     message: "Job Created",
     data: job,
+  });
+};
+
+// Update a Job => /api/v1/job/:id
+exports.updateJob = async (req, res, next) => {
+  let job = await Job.findById(req.params.id);
+
+  if (!job) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found.",
+    });
+  }
+
+  job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Job is updated",
+    data: job,
+  });
+};
+
+// Search jobs with radius  => /api/v1/jobs/:zipcode/:distance
+exports.getJobsInRadius = async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // getting latitude and longitude from geocoder with zipcode
+  // here we dont want to match with the exact values that's why we didn't use ?query=
+
+  const loc = await geoCoder.geocode(zipcode);
+  const latitude = loc[0].latitude;
+  const longitude = loc[0].longitude;
+
+  const radius = distance / 3963; // in miles
+
+  const jobs = await Job.find({
+    location: {
+      $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    results: jobs.length,
+    data: jobs,
   });
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 //Importing jobs controller methods
-const { getJobs, newJob } = require("../controllers/jobsController.js");
+const {
+  getJobs,
+  newJob,
+  getJobsInRadius,
+  updateJob,
+} = require("../controllers/jobsController.js");
 
 router.route("/jobs").get(getJobs);
-router.route("/jobs/new").post(newJob);
+router.route("/job/new").post(newJob);
+router.route("/jobs/:zipcode/:distance").get(getJobsInRadius);
+router.route("/job/:id").put(updateJob);
 
 module.exports = router;
